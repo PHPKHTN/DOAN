f13ac43c70253cd64e86e5bd1773710618783af4
PhungHung <happyboy1503@gmail.com>
2017-11-16 23:32:05 +0700

    admin đã kết nối được

---
+++ admin/Smarty/templates/logo_profile.tpl
@@ -0,0 +1,9 @@
+<a href="quantri.php">
+<img id="logo" src="public/layout/resources/images/alphatek.png" alt="Simpla Admin logo" /></a> 
+<!-- Sidebar Profile links -->
+<div id="profile-links"> Chào, <a href="#" title="Edit your profile">
+{$smarty.session.fullname}
+</a><br />
+  <br />
+  <a href="#" title="View the Site">View the Site</a> | 
+  <a href="quantri.php?func=exit" title="Sign Out">Sign Out</a> </div>
---
+++ admin/Smarty/templates_c/87fc0fa10e59ccc78dbb5dd00a6434b0a0238e30.file.content_left.tpl.php
@@ -0,0 +1,40 @@
+<?php /* Smarty version Smarty-3.1.14, created on 2017-11-14 13:19:43
+         compiled from "Smarty\templates\content_left.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:6005a0adf5fafc8b2-79629959%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '87fc0fa10e59ccc78dbb5dd00a6434b0a0238e30' => 
+    array (
+      0 => 'Smarty\\templates\\content_left.tpl',
+      1 => 1418697286,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '6005a0adf5fafc8b2-79629959',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.14',
+  'unifunc' => 'content_5a0adf5fb00730_61573554',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5a0adf5fb00730_61573554')) {function content_5a0adf5fb00730_61573554($_smarty_tpl) {?>
+<div class="content-box column-left">
+  <div class="content-box-header">
+    <h3>Content box left</h3>
+  </div>
+  <!-- End .content-box-header -->
+  
+  <div class="content-box-content">
+    <div class="tab-content default-tab">
+      <h4>Maecenas dignissim</h4>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in porta lectus. Maecenas dignissim enim quis ipsum mattis aliquet. Maecenas id velit et elit gravida bibendum. Duis nec rutrum lorem. Donec egestas metus a risus euismod ultricies. Maecenas lacinia orci at neque commodo commodo. </p>
+    </div>
+    <!-- End #tab3 --> 
+    
+  </div>
+  <!-- End .content-box-content --> 
+  
+</div>
+<!-- End .content-box --><?php }} ?>
